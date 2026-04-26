a7bffab02d746ad5f9b8d95c4e2542c45d7566fa
Monali Gupta <monaligupta06@gmail.com>
2026-04-26 15:59:45 +0530

    final

--- app.js
+++ app.js
@@ -13,20 +13,29 @@ const addressRoutes = require("./routes/AddressRouter");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "https://ecommerce-frontend-cyan-ten.vercel.app" 
-    ],
-    credentials: true
-  })
-);
-// app.options("*", cors()); 
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://ecommerce-frontend-cyan-ten.vercel.app"
+  ],
+  credentials: true
+}));
 
 app.use(cookieParser());
 app.use(express.json());
+
+const connectDB = async () => {
+  if (mongoose.connections[0].readyState) return;
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("DB Connected");
+};
+
+
+app.use(async (req, res, next) => {
+  await connectDB();
+  next();
+});
 
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
@@ -34,18 +43,11 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/addresses", addressRoutes);
 
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("DB Connected Successfully");
-    
-    app.listen(process.env.PORT || 8080, () => {
+// For local development
+if (process.env.NODE_ENV !== "production") {
+  app.listen(process.env.PORT || 8080, () => {
     console.log("Server running on:", process.env.PORT || 8080);
-     })
-  })
-  .catch((error) => {
-    console.log("DB Connection Failed", error.message);
   });
+}
 
-module.exports = app; 
+module.exports = app;
